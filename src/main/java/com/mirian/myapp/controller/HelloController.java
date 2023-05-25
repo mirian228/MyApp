29e1949a872f1f2361eccad7c4691a949fd37f99
mirian228 <pariksini@mail.ru>
2023-05-25 11:22:07 +0400

    Starter project

---
+++ src/main/java/com/mirian/myapp/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.mirian.myapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World";
+    }
+
+
+
+
+
+}
