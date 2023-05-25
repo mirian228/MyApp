d4307727fef36cab8c2c8fc623f57558421ff145
mirian228 <pariksini@mail.ru>
2023-05-25 14:18:51 +0400

    @Scope annotation

--- src/main/java/com/mirian/myapp/dependencyinjection/CricketCoach.java
+++ src/main/java/com/mirian/myapp/dependencyinjection/CricketCoach.java
@@ -1,10 +1,13 @@
 package com.mirian.myapp.dependencyinjection;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 @Primary
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON) // BY DEFAULT IT IS ALWAYS SINGLETON
 public class CricketCoach implements Coach{
     @Override
     public String getDailyWorkout() {
