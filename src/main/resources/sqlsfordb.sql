55959e6f3a0cd16d4184e976d491837ee1b6e7c5
mirian228 <pariksini@mail.ru>
2023-05-26 12:38:01 +0400

    Added sql script for creating table

---
+++ src/main/resources/sqlsfordb.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE student;
+USE student;
+
+
+CREATE TABLE student (
+	 id int NOT NULL AUTO_INCREMENT,
+    first_name varchar(45) DEFAULT NULL,
+	last_name varchar(45) DEFAULT NULL,
+    email varchar(45) DEFAULT NULL,
+	PRIMARY KEY (id)
+) ENGINE = InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
