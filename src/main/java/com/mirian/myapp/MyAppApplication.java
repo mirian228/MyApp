588bda7a8290a8f904e5aedafa141607d72fa2d4
mirian228 <pariksini@mail.ru>
2023-05-26 12:42:08 +0400

    Added Spring Boot CommandLineRunner

--- src/main/java/com/mirian/myapp/MyAppApplication.java
+++ src/main/java/com/mirian/myapp/MyAppApplication.java
@@ -1,7 +1,9 @@
 package com.mirian.myapp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MyAppApplication {
@@ -9,5 +11,14 @@ public class MyAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MyAppApplication.class, args);
 	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner -> {
+			System.out.println("Hello World");
+		};
+	}
+
+
 
 }
